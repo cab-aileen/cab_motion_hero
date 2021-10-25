824dddd8333c2dfefffffeccf02f134e4b5b6810
cab-aileen <cabreraaileen3@gmail.com>
2021-10-25 02:39:02 -0400

    added hero logo and timeline

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,4 +9,13 @@ const mainTL = gsap.timeline();
 // to = end point of timeline
 // mainTL.to("#hero article",{duration:2,alpha:0, scale:2});
 
+const heroHeight = document.querySelector("#hero");
 
+const aniTime = 0.5;
+
+console.log(heroHeight.clientHeight + "px is the height of the hero section");
+console.log(heroHeight.clientWidth + "px is the width of the hero section");
+
+mainTL.from("#hero >div article",{duration:1, alpha:0, y:-heroHeight.clientHeight})
+    .from("#hero h1",{duration:0.5, alpha:0, y:"+=200", rotation:180}, "madeUp")
+    .from("#hero h2",{duration:1, alpha:0, y:"+=200"}, "madeUp");
