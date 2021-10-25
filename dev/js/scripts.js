3befc5a012da3956c2bd08e1ad44673f0cdf93c7
cab-aileen <cabreraaileen3@gmail.com>
2021-10-25 01:49:25 -0400

    gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,12 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+const mainTL = gsap.timeline();
+
+// reference the timeline | Do we want to start at the beginning or the end of the timeline? | What thing to you want to animate? | duration of the animation? | What do we want to do?
+
+// from = start point of timeline
+// mainTL.from("#hero article",{duration:2,alpha:0, scale:2});
+
+// to = end point of timeline
+// mainTL.to("#hero article",{duration:2,alpha:0, scale:2});
+
+
