91366927d14cab0c0227054aea0966206e1dfff6
cab-aileen <cabreraaileen3@gmail.com>
2021-11-10 01:53:57 -0500

    updated svg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,22 +1,78 @@
 import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
+
 const mainTL = gsap.timeline();
 
-// reference the timeline | Do we want to start at the beginning or the end of the timeline? | What thing to you want to animate? | duration of the animation? | What do we want to do?
+function frontshoe(){
+    const tl = gsap.timeline();
 
-// from = start point of timeline
-// mainTL.from("#hero article",{duration:2,alpha:0, scale:2});
+    tl.to("#front",{duration:1, y:"-=-850"})
+    return tl;
+}
 
-// to = end point of timeline
-// mainTL.to("#hero article",{duration:2,alpha:0, scale:2});
+function backshoe(){
+    const tl =gsap.timeline();
+    tl.to("#back", {duration:1, y:"-=-850"})
+}
 
-const heroHeight = document.querySelector("#hero");
+mainTL.add(frontshoe())
+        .add(backshoe());
 
-const aniTime = 0.5;
 
-console.log(heroHeight.clientHeight + "px is the height of the hero section");
-console.log(heroHeight.clientWidth + "px is the width of the hero section");
 
-mainTL.from("#hero >div article",{duration:1, alpha:0, y:-heroHeight.clientHeight})
-    .from("#hero h1",{duration:0.5, alpha:0, y:"+=200", rotation:180}, "madeUp")
-    .from("#hero h2",{duration:1, alpha:0, y:"+=200"}, "madeUp")
-    .from("#hero aside",{duration:2, alpha:0, scale:4});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GSDevTools.create();
+
+//const heroHeight = document.querySelector("#hero");
+
+//const aniTime = 0.5;
+
+//console.log(heroHeight.clientHeight + "px is the height of the hero section");
+//console.log(heroHeight.clientWidth + "px is the width of the hero section");
+
+//mainTL.from("#hero >div article",{duration:1, alpha:0, y:-heroHeight.clientHeight})
+//    .from("#hero h1",{duration:0.5, alpha:0, y:"+=200", rotation:180}, "madeUp")
+//    .from("#hero h2",{duration:1, alpha:0, y:"+=200"}, "madeUp")
+//    .from("#hero aside",{duration:2, alpha:0, scale:4});
