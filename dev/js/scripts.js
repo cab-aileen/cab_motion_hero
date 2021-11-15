1fb9cc2837042905a5b2aa44aae882aae3a45913
cab-aileen <cabreraaileen3@gmail.com>
2021-11-14 19:45:38 -0500

    splitboots

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,42 +7,76 @@ import { MotionPathHelper } from "gsap/MotionPathHelper";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin, Flip, MotionPathHelper, MotionPathPlugin);
 
+const mainTL = gsap.timeline();
+const state = Flip.getState("#boots-8, #boots-9");
 
-function shoesdropping(){
+function shoesdroppingwiththud(){
+    const tl =gsap.timeline();
+    //tl.from("#shoes-3",{duration:1,y:"-=1300", ease:"none"})
+    tl.from("#front-3",{duration: 1, y:"-=1300", ease: "bounce.out", stagger:0.25})
+    //.from("#thud-illustration", { },"-=50%")
+    .from("#thud-illustration", {alpha:0, duration:.02},"-=.2")
+    //.to("#front-3",{rotation:-28, y:"-=-212"});
+
     
-    const mainTL = gsap.timeline();
-    mainTL.to("#shoes-2",{duration:1,scale:3,y:"-=600"});
+    return tl;
+}
+
+function bootsrotatingwithsquishandthud(){
+    const tl =gsap.timeline();
+
+    Flip.to(state, {duration: 1,fade: true,ease: "power1.inOut",absolute: true});
+
+
+    
+
+    return tl;
+    
+}
+
+function linesplitbetweenboots(){ //scaleX:100
+    const tl =gsap.timeline();
+    tl.from("#leftline-1",{duration:1.2,drawSVG:0,ease:"none"})
+    .from("#rightline-1",{duration:1.2,drawSVG:0,ease:"none"})
+    .to("#leftline-1",{duration:2,x:"-=1300"},"sametime")
+    .to("#rightline-1",{duration:2,x:"-=-1300"},"sametime")
+
+    return tl;
+}
+
+function bootssplitinhalf(){
+    const tl=gsap.timeline();
+
+    tl.to
+}
+
+function drawwolf(){
+    const tl =gsap.timeline();
+
+    return tl;
+}
+
+function spikesrotating(){
+    const tl =gsap.timeline();
+
+    return tl;
+}
+
+function toplinewolfcut(){
+    const tl =gsap.timeline();
+
+    return tl;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mainTL.add(shoesdroppingwiththud())
+        .add(bootsrotatingwithsquishandthud())
+        .add(linesplitbetweenboots())
+        .add(bootssplitinhalf())
+        .add(drawwolf())
+        .add(spikesrotating())
+        .add(toplinewolfcut());
+        
 
 
 
